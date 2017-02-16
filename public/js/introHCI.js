63ac080adf3eca0be01df1b07351d5004aa710d9
Timothy Wang <wangtim1996@gmail.com>
2017-02-16 10:31:15 -0800

    finished lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,29 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	var url = "/project/" + idNumber;
+	$.get(url, addDesc);
+
+	console.log(url);
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function addDesc(result) {
+	console.log(result);
+	var query = "#project" + result['id'] + " .details";
+	var detailHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+		'<h3>' + result['date'] + '</h3>' +
+		'<p>' + result['summary'] + '</p>';
+	$(query).html(detailHTML);
+
+	/*
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>';
+		*/
 }
 
 /*
@@ -35,4 +57,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", changeColor);
+}
+
+function changeColor(result){
+	console.log(result);
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
